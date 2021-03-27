695b9b01dc38128b03ec919a13555fe5612a6a0d
FantasticBoyZ <khoidragonslayer@gmail.com>
2021-03-27 22:34:56 +0700

    first commit

---
+++ LoginPageDemo1/Controllers/UserLoginController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LoginPageDemo1.Models;
+using System.Data;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace LoginPageDemo1.Controllers
+{
+    public class UserLoginController : Controller
+    {
+        // GET: UserLogin
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(LoginClass lc) {
+            string mainConn = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            SqlConnection sqlconn = new SqlConnection(mainConn);
+            string sqlquery = "select username,[password] from [User] where username = @username and password = @password";
+            sqlconn.Open();
+            SqlCommand sqlComm = new SqlCommand(sqlquery,sqlconn);
+            sqlComm.Parameters.AddWithValue("@username", lc.UserName);
+            sqlComm.Parameters.AddWithValue("@password", lc.Password);
+            SqlDataReader sdr = sqlComm.ExecuteReader();
+            if (sdr.Read()) {
+                Session["Username"] = lc.UserName.ToString();
+                return RedirectToAction("welcome");
+            }
+            else {
+                ViewData["Message"] = "User Login Detail Fail!";
+            }
+            sqlconn.Close();
+            return View();
+        }
+        public ActionResult welcome() {
+            return View();
+        }
+    }
+}
---
+++ LoginPageDemo1/Models/LoginClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace LoginPageDemo1.Models {
+    public class LoginClass {
+        [Required(ErrorMessage ="Please Enter Your Username")]
+        [Display(Name ="Enter Username : ")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "Please Enter Your Password")]
+        [Display(Name = "Enter Password : ")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
